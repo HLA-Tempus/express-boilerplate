6693a0867dfe9f2ab72ffe4ba354d31b63b70d04
Park Changyong <tempus157@gmail.com>
2022-05-11 02:56:43 +0900

    use urlencoded

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 import { router } from "~/routes";
 
 const app = express();
-app.use(express.json());
-app.use("/", router);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
 app.listen(3000, () => {
   console.log("Server has been started...");
 });
